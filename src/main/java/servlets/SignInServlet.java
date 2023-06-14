fe34d38513e909bb710ac3eeca7db910c9dd8823
artem <iakonien@gmail.com>
2023-06-14 21:04:48 +0300

    Initial commit

---
+++ src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+public class SignInServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignInServlet(AccountService accountService){
+        this.accountService = accountService;
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        boolean trouble = true;
+
+
+        if(login != null && password != null){
+            UserProfile user = accountService.getUserByLogin(login);
+            if(user != null){
+                if(Objects.equals(user.getPass(), password)){
+                    trouble = false;
+                }
+            }
+        }
+
+        if(!trouble){
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("Authorized: " + login);
+        } else {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            resp.getWriter().println("Unauthorized");
+        }
+
+    }
+
+
+}
